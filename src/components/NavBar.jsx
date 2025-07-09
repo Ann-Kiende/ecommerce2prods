036b045d734782dcbf9824b1022c75ebbf0d34fe
Kitanna <Karl-Kitanna@Admins-MacBook-Pro.local>
2025-07-09 18:25:17 +0300

    Update NavBar jsx and css files with content and styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import './NavBar.css';
-import smalllogo from '../assets/smalllogo.png';
+import smalllogo2 from '../assets/smalllogo2.png';
 
 const NavBar = () => {
   return (
     <div className='NavBar'>
-      <div className='logo'>
-        <img src={smalllogo}></img>
+      {/* Logo */}
+      <a href='/' className='logo'>
+        <img src={smalllogo2}></img>
+      </a>
+
+      {/* Desktop Nav  */}
+      <div className='nav-links'>
+        <a href='/'>Home</a>
+        <a href='/' className='active'>
+          Products
+        </a>
+        <a href='/'>About</a>
       </div>
+
+      {/* Right Side Nav */}
+      <div className='nav-right'>
+        <input type='text' name='' id='search-bar' placeholder='Search...' />
+        <div className='cart'>
+          🛍️<span>0</span>
+        </div>
+        <button className='login-btn'>Login</button>
+      </div>
+
+      {/* Mobile Menu */}
+      <div className='hamburger'>☰</div>
     </div>
   );
 };
