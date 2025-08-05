932b60d657cd6e42dce663f0003103f8ad48f319
Ann-Kiende <annkiende29@gmail.com>
2025-08-05 15:48:48 +0300

    Refactor: restructure App component for routing Implement routing setup with react-router-dom Replace Card component with proper layout structure Add navigation and page components (Home, About) Remove obsolete code references

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
 import React from 'react';
 import './App.css';
 import Card from './components/Card';
+import Home from './components/Home';
+import About from './components/About';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className='App'>
-      <Card />
+      {/* <Card /> */}
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/products' element={<Card />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
