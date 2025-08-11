b67eba95ddfed5e1beb964a606c6fcd29fa779f0
Ann-Kiende <annkiende29@gmail.com>
2025-08-11 06:36:46 +0300

    feat: scaffold dropdown.jsx file, add dropdown.css file and add callcenter.jpg image

---
+++ src/components/dropdown.jsx
@@ -0,0 +1,12 @@
+import { React, useState } from 'react';
+import './dropdown.css';
+
+const dropdown = () => {
+  return (
+    <div className='dropdown-btn'>
+      <div className='dropdown-heading'>name</div>
+    </div>
+  );
+};
+
+export default dropdown;
