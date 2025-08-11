8e54423e05eb8d9d62832c9b8ff84359e0160324
Ann-Kiende <annkiende29@gmail.com>
2025-08-11 12:35:30 +0300

    chore: update routes for  correct navigation to contact component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import smalllogo2 from '../assets/smalllogo2.png';
 import { FaBars } from 'react-icons/fa6';
 import { MdClose } from 'react-icons/md';
 import { NavLink, Link } from 'react-router-dom';
-import dropdown from './Dropdown';
+// import dropdown from './Dropdown';
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -40,7 +40,7 @@ const NavBar = () => {
           Products
         </NavLink>
         <NavLink
-          to='/dropdown'
+          to='/contact'
           className={({ isActive }) => (isActive ? 'active' : '')}
           onClick={closeMobileMenu}
         >
