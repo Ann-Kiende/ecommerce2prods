4cef4f3d3a2b209234713b13fac1a74528582674
Ann-Kiende <annkiende29@gmail.com>
2025-08-11 10:23:37 +0300

    feat(dropdown): add reusable component with title, icon, and collapsible list support

--- src/components/dropdown.jsx
+++ src/components/dropdown.jsx
@@ -1,9 +1,9 @@
 import { React, useState } from 'react';
 import './Dropdown.css';
-import { BsFillCartCheckFill } from 'react-icons/bs';
+// import { BsFillCartCheckFill } from 'react-icons/bs';
 import { FaPlus, FaMinus } from 'react-icons/fa';
 
-const Dropdown = () => {
+const Dropdown = (props) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -13,33 +13,67 @@ const Dropdown = () => {
   return (
     <div className='dropdown-btn'>
       <div className='dropdown-heading' onClick={toggleMenu}>
-        <div className='dd-img'>
-          <BsFillCartCheckFill />
-        </div>
-        <div className='dd-title'>How to track your order?</div>
+        <div className='dd-img'>{props.icon}</div>
+        <div className='dd-title'>{props.title}</div>
         <div className='dd-toggle'>{isOpen ? <FaMinus /> : <FaPlus />}</div>
       </div>
 
       {isOpen && (
         <div className='dropdown-content'>
           <ul className='dd-ul'>
-            <li className='dd-li'>
-              Click the drop-down arrow beside your name and select Orders.
-            </li>
-            <li className='dd-li'>
-              Click <strong>SEE DETAILS</strong> in front of the order you want
-              to track.
-            </li>
-            <li className='dd-li'>
-              Click <strong>TRACK MY ITEM</strong> button in front of the order
-              you wish to track.
-            </li>
-            <li className='dd-li'>Delivery history is displayed.</li>
+            {Array.isArray(list) ? (
+              list.map((item, index) => (
+                <li key={index} className='dd-li'>
+                  {item}
+                </li>
+              ))
+            ) : (
+              <li className='dd-li'>{list}</li>
+            )}
           </ul>
         </div>
       )}
     </div>
   );
 };
+
+// const Dropdown = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const toggleMenu = () => {
+//     setIsOpen(!isOpen);
+//   };
+
+//   return (
+//     <div className='dropdown-btn'>
+//       <div className='dropdown-heading' onClick={toggleMenu}>
+//         <div className='dd-img'>
+//           <BsFillCartCheckFill />
+//         </div>
+//         <div className='dd-title'>How to track your order?</div>
+//         <div className='dd-toggle'>{isOpen ? <FaMinus /> : <FaPlus />}</div>
+//       </div>
+
+//       {isOpen && (
+//         <div className='dropdown-content'>
+//           <ul className='dd-ul'>
+//             <li className='dd-li'>
+//               Click the drop-down arrow beside your name and select Orders.
+//             </li>
+//             <li className='dd-li'>
+//               Click <strong>SEE DETAILS</strong> in front of the order you want
+//               to track.
+//             </li>
+//             <li className='dd-li'>
+//               Click <strong>TRACK MY ITEM</strong> button in front of the order
+//               you wish to track.
+//             </li>
+//             <li className='dd-li'>Delivery history is displayed.</li>
+//           </ul>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
 
 export default Dropdown;
