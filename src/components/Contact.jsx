fd4673e1d7b96a7c90f7ad9e766c3f0799d7ae00
Ann-Kiende <annkiende29@gmail.com>
2025-08-12 10:37:05 +0300

    feat(Contact): add order action icons Import TbShoppingCartCancel and TbTruckReturn

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,6 +3,8 @@ import './Contact.css';
 import callCenter from '../assets/callcenter.jpg';
 import Dropdown from './Dropdown';
 import { BsFillCartCheckFill } from 'react-icons/bs';
+import { TbShoppingCartCancel } from 'react-icons/tb';
+import { TbTruckReturn } from 'react-icons/tb';
 
 const Contact = () => {
   return (
@@ -16,15 +18,15 @@ const Contact = () => {
           </p>
           <p>
             We are available from Monday to Sunday between 9 am to 6 pm on Live
-            Chats
+            Chats.
           </p>
           <p>
             For other inquiries, you can also reach us on +254700 000 000 from
-            Monday to Friday between 9am to 6pm on calls
+            Monday to Friday between 9am to 6pm on calls.
           </p>
           <p>
             To place an order, call us on +254711 011 011 or WhatsApp us on
-            +254700 000 000 from Monday to Sunday between 8am - 8pm
+            +254700 000 000 from Monday to Sunday between 8am - 8pm.
           </p>
         </div>
         <div className='c-img'>
@@ -39,27 +41,40 @@ const Contact = () => {
             title='How to track your order?'
             list={[
               'Click the drop-down arrow beside your name and select Orders.',
-              'Click <strong>SEE DETAILS</strong> in front of the order you want',
-              'Click <strong>TRACK MY ITEM</strong> button in front of the order you wish to track',
-              'Delivery history is displayed.',
+              <>
+                Click <strong>SEE DETAILS</strong> in front of the order you
+                want.
+              </>,
+              <>
+                Click <strong>TRACK MY ITEM</strong> button in front of the
+                order you wish to track.
+              </>,
+              'Delivery history is displayed',
             ]}
           />
 
           <Dropdown
-            icon={<BsFillCartCheckFill />}
+            icon={<TbShoppingCartCancel />}
             title='How to cancel your order?'
             list={[
-              'From the list of Orders in your Jumia Account, Click SEE DETAILS in front of the order you wish to cancel.',
+              <>
+                From the list of Orders in your Jumia Account, Click{' '}
+                <strong>SEE DETAILS</strong> in front of the order you wish to
+                cancel.
+              </>,
               'Click the Cancel Item button.',
               'Select the right quantity and cancellation reason from the drop-down list and submit.',
             ]}
           />
 
           <Dropdown
-            icon={<BsFillCartCheckFill />}
+            icon={<TbTruckReturn />}
             title='How to return your order?'
             list={[
-              'Login and go to ORDERS Then click on the order of the item(s) you wish to return.',
+              <>
+                Login and go to <strong>ORDERS</strong> Then click on the order
+                of the item(s) you wish to return.
+              </>,
               'Select the number of items you wish to return, the reason for the return, and give us more details to help us identify the issue with the product.',
               'Select your preferred refund method Click here to know more about the refund possibilities.',
               'Check your information and submit your return request.',
